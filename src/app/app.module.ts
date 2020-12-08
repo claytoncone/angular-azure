2ceaead66dada8978b7c25c2946c40f26d590386
claytoncone <ronald.trask@lumen.com>
2020-12-08 12:14:53 -0700

    INITIAL CHECKIN

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import {HttpClientModule} from '@angular/common/http';
+import {MsalModule} from '@azure/msal-angular'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    MsalModule.forRoot({
+      auth:{
+        clientId:"380bcae5-7822-4b2c-a2c9-b686e25d10d2", // Application Id of Application registered in B2C
+        authority:"https://idmgr.b2clogin.com/idmgr.onmicrosoft.com/B2C_1_SignInSignUp", //signup-signin userflow
+        // https://idmgr.b2clogin.com/idmgr.onmicrosoft.com/<policy-name>/oauth2/v2.0/authorize
+        //https://jitbox.b2clogin.com/jitbox.onmicrosoft.com/B2C_1_susi", //signup-signin userflow
+        validateAuthority:false,
+        redirectUri:"http://localhost:4200/"
+      },
+      cache:{
+        cacheLocation:"sessionStorage",
+        storeAuthStateInCookie:false
+      }
+    },{
+      consentScopes:[
+        "user.read","openid","profile"
+      ]
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
